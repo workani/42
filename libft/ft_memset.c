5236836b0757d57c5bb13a61206be14ca9cf8066
workani <tomdanil68@gmail.com>
2025-06-03 17:06:10 +0200

    Implement ft_isalnum, ft_isprint, ft_tolower, ft_toupper, ft_strlen and  ft_memset

---
+++ libft/ft_memset.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dklepenk <dklepenk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/03 15:51:36 by dklepenk          #+#    #+#             */
+/*   Updated: 2025/06/03 17:04:46 by dklepenk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void *ft_memset(void *s, int c, size_t n)
+{
+    unsigned char uc;
+    unsigned char *str;
+
+    uc = (unsigned char) c;
+    str = (unsigned char*) s;
+    
+    while (n-- && *str != 0)
+        *str++ = uc;
+    
+    return (s);
+}
+
+// int main()
+// {
+//     char str[] = "Hello world!";
+//     printf("before: %s\n", str);
+    
+//     ft_memset(str, '$', 5);
+//     printf("after: %s\n", str);
+// }
