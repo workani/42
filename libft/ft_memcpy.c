07b721171a9b65b08ba19229b6ff4d9b7a9191c1
workani <tomdanil68@gmail.com>
2025-06-03 20:25:44 +0200

    Implement ft_memcpy

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,18 +6,31 @@
 /*   By: dklepenk <dklepenk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 16:50:23 by dklepenk          #+#    #+#             */
-/*   Updated: 2025/06/03 16:54:00 by dklepenk         ###   ########.fr       */
+/*   Updated: 2025/06/03 20:23:46 by dklepenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include <stdio.h>
 
-void ft_bzero(void *s, size_t n)
+void *ft_memcpy(void *dst, const void *src, size_t n)
 {
-    int i;
-    unsigned char *str;
+    unsigned char *dest = (unsigned char*) dst;
+    unsigned char *source = (unsigned char*) src;
     
-    i = 0;
-    str = (unsigned char) s;
-    while(str[i] != '\0')
+    while (n--)
+        *dest++ = *source++;
+    return (dst);
+}
+
+
+int main()
+{
+    int n = 6;
+    int dst[n];
+    int src[] = {6, 2, 5, 5, 3, 1};
+    
+    ft_memcpy(dst, src, 24);
+
+    for(int i = 0; i < n; i++)
+        printf("arr[%d]: %d\n",i + 1, dst[i]);
 }
