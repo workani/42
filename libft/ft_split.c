9de5e09a1d8d51c84ec38e4663a135ffcfe3303c
workani <tomdanil68@gmail.com>
2025-06-11 19:47:05 +0200

    Revrite split from the scratch

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: dklepenk <dklepenk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 18:08:03 by dklepenk          #+#    #+#             */
-/*   Updated: 2025/06/10 22:45:02 by dklepenk         ###   ########.fr       */
+/*   Updated: 2025/06/11 19:46:51 by dklepenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,91 +46,73 @@ char *ft_cst_strncpy(char *dest, char *src, int n)
 
 char **ft_split(char const *s, char c)
 {
-	int s_idx;
-	int last_split_idx;
+	int i;
+	int last_word_idx;
 	int strs_idx;
-	int words_count;
 	char **strs;
-	
-	s_idx = 0;
-	last_split_idx = 0;
+
+	i = 0;
 	strs_idx = 0;
-	words_count = ft_words_count((char*) s, c);
-	strs = (char **) malloc(words_count * sizeof(char*));
-	while (s[s_idx])
+	strs = (char **) malloc(ft_words_count((char*) s, c) * sizeof(char*));
+	while (s[i])
 	{
-		if (s[s_idx] == c|| s[s_idx + 1] == '\0')
+		if (s[i] != c && s[i] != '\0')
 		{
-			strs[strs_idx] = malloc((s_idx - last_split_idx) + 1);
-			if (last_split_idx == 0)
-			{
-				ft_cst_strncpy(strs[strs_idx], (char*) s, s_idx);
-				last_split_idx = s_idx + 1;                                                               
-			}                                                                                                       
-			else
-			{
-				if (s[s_idx + 1] == '\0')
-					s_idx++;
-				ft_cst_strncpy(strs[strs_idx], (char*) s + last_split_idx, s_idx - last_split_idx);
-				last_split_idx = s_idx + 1;
-			}
+			last_word_idx = i;
+			while (s[i] != c && s[i] != '\0')
+				i++;
+			strs[strs_idx] = malloc((i - last_word_idx) + 1);
+			ft_cst_strncpy(strs[strs_idx], (char*) s + last_word_idx, i - last_word_idx);
 			strs_idx++;
 		}
-		s_idx++;
+		i++;
 	}
 	return (strs);
 }
 
-// int main()
-// {
-// 	char **strs;
-	
-// 	strs = ft_split("hello,world", ',');
-// 	printf("%s %s", strs[0], strs[1]);
-// }
 
 int main() {
 	char **strs;
 
 	// Test 1
-	strs = ft_split("hello!", ' ');
-	printf("Test 1: Input = \"hello!\", Sep = ' '\nExpected:\nhello!\nGot:\n");
-	for (int i = 0; i < 1; i++)
-		printf("%s\n", strs[i]);
+	// strs = ft_split("hello, world, this is workani", ',');
+	// for (int i = 0; i < 3; i++)
+	// 	printf("%s\n", strs[i]);
+	
 
-		// Test 3
-	strs = ft_split("hello!zzzzzzzz", 'z');
-	printf("Test 3: Input = \"hello!zzzzzzzz\", Sep = 'z'\nExpected:\nhello!\nGot:\n");
-	for (int i = 0; i < 1; i++)
-		printf("%s\n", strs[i]);
+	// 	// Test 3
+	// strs = ft_split("bye,world", ',');
+	// printf("Test 3: Input = \"hello!zzzzzzzz\", Sep = 'z'\nExpected:\nhello!\nGot:\n");
+	// for (int i = 0; i < 2; i++)
+	// 	printf("%s\n", strs[i]);
 
 	// Test 4
-	strs = ft_split("\11\11\11\11hello!\11\11\11\11", '\11');  // \11 = tab
-	printf("Test 4: Input = \"\\11\\11\\11\\11hello!\\11\\11\\11\\11\", Sep = '\\11'\nExpected:\nhello!\nGot:\n");
-	for (int i = 0; i < 1; i++)
-		printf("%s\n", strs[i]);
+	// strs = ft_split("\11\11\11\11hello!\11\11\11\11", '\11');  // \11 = tab
+	// printf("Test 4: Input = \"\\11\\11\\11\\11hello!\\11\\11\\11\\11\", Sep = '\\11'\nExpected:\nhello!\nGot:\n");
+	// for (int i = 0; i < 1; i++)
+	// 	printf("%s\n", strs[i]);
 
-	// Test 5
-	strs = ft_split("", 'a');
-	printf("Test 5: Input = \"\", Sep = 'a'\nExpected:\n(nothing)\nGot:\n");
-	// Should be no output
+// 	// Test 5
+// 	strs = ft_split("", 'a');
+// 	printf("Test 5: Input = \"\", Sep = 'a'\nExpected:\n(nothing)\nGot:\n");
+// 	// Should be no output
 
-	// Test 6
-	strs = ft_split("ggggggggggg", 'g');
-	printf("Test 6: Input = \"ggggggggggg\", Sep = 'g'\nExpected:\n(nothing)\nGot:\n");
-	// Should be no output
+// 	// Test 6
+// 	strs = ft_split("ggggggggggg", 'g');
+// 	printf("Test 6: Input = \"ggggggggggg\", Sep = 'g'\nExpected:\n(nothing)\nGot:\n");
+// 	// Should be no output
 
-	// Test 7
-	strs = ft_split("^^^1^^2a,^^^^3^^^^--h^^^^", '^');
-	printf("Test 7: Input = \"^^^1^^2a,^^^^3^^^^--h^^^^\", Sep = '^'\nExpected:\n1\n2a,\n3\n--h\nGot:\n");
-	for (int i = 0; i < 4; i++)
-		printf("%s\n", strs[i]);
+// 	// Test 7
+	// strs = ft_split("^^^1^^2a,^^^^3^^^^--h^^^^", '^');
+	// printf("Test 7: Input = \"^^^1^^2a,^^^^3^^^^--h^^^^\", Sep = '^'\nExpected:\n1\n2a,\n 3\n--h\nGot:\n");
+// 	for (int i = 0; i < 4; i++)
+// 		printf("%s\n", strs[i]);
 
 	// Test 8
-	strs = ft_split("nonempty", '\0');
-	printf("Test 8: Input = \"nonempty\", Sep = '\\0'\nExpected:\nnonempty\nGot:\n");
-	for (int i = 0; i < 1; i++)
-		printf("%s\n", strs[i]);
+	// strs = ft_split("nonempty", '\0');
+	// printf("Test 8: Input = \"nonempty\0\", Sep = '\\0'\nExpected:\nnonempty\nGot:\n");
+	// for (int i = 0; i < 1; i++)
+	// 	printf("%s\n", strs[i]);
 
 	return 0;
 }
