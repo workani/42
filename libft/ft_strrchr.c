b8f5e427ebf997fcbafb356364f1125352b69606
workani <tomdanil68@gmail.com>
2025-06-12 21:35:57 +0200

    Fix ft_strrchr

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,24 +6,29 @@
 /*   By: dklepenk <dklepenk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 15:08:05 by dklepenk          #+#    #+#             */
-/*   Updated: 2025/06/04 15:14:26 by dklepenk         ###   ########.fr       */
+/*   Updated: 2025/06/12 21:35:21 by dklepenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strchr(const char *s, int c)
+char *ft_strrchr(const char *s, int c)
 {
+    int i;
     char *occurrence;
     
+    i = 0;
+    c = c % 128; // prevent overflow
     occurrence = NULL;
-    while (*s)
+    while (s[i])
     {
-        if (*s == c)
-            occurrence = (char *)s;
-        s++;
+        if (s[i] == c)
+            occurrence = (char *)s + i;
+        i++;
     }
-    return (occurrence);
+    if (c == '\0')
+        occurrence = (char*) s + i;
+    return (char *)(occurrence);
 }
 
 // int main()
